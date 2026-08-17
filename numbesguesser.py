872df22f65dfc496dd54aa25d07300de4504fdd5
Zaid Athani <athani.zaid14@gmail.com>
2026-08-17 12:22:20 +0530

    Update numbesguesser.py

--- numbesguesser.py
+++ numbesguesser.py
@@ -1,20 +1,22 @@
+#number guessing from 1 to 10
+
 import random
-j= random.randint(1,5)
+j= random.randint(1,10)
 
 
 while True:
     print ("enter your guess")
     i=int(input())
     if i==j:
-        print ("correct")
-        guess=True
+        print ("your guess is correct")
+        
         break
     elif i<=j%2:
-        print ("your guess is lower")
+        print ("your guess is low, the value is bit higher")
         
     elif i>=j%2:
         print ("your guess is too high")
         
     elif i==j%2:
-        print ("your guess in in between") 
-   
+        print ("your guess is in between") 
+
